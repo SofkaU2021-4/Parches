12137db6415a9a838bf73159d34a0be10446838d
Cristian Cabarcas <cridejecato@gmail.com>
2022-01-31 17:32:46 -0500

    Feat: Router and Use Case

--- src/main/java/co/com/sofka/parches/routers/RouterParche.java
+++ src/main/java/co/com/sofka/parches/routers/RouterParche.java
@@ -1,4 +1,29 @@
 package co.com.sofka.parches.routers;
 
+import co.com.sofka.parches.dtos.ParcheDTO;
+import co.com.sofka.parches.useCases.ObtenerParchesUseCase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+@RequestMapping
 public class RouterParche {
+    @Bean
+    public RouterFunction<ServerResponse> obtenerParches(ObtenerParchesUseCase obtenerParchesUseCase){
+        return route(
+                GET("/api/parches").and(accept(MediaType.APPLICATION_JSON)),
+                request -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromPublisher(obtenerParchesUseCase.apply(), ParcheDTO.class))
+        );
+    }
 }
