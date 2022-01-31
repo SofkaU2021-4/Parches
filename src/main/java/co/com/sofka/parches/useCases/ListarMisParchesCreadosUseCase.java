b451629cbf9894f4a7abf483d259322636d11c0a
Tavo826 <gustavo.orozcom68@gmail.com>
2022-01-31 17:13:15 -0500

    feat: se agrega función para contar participantes en el caso de uso

--- src/main/java/co/com/sofka/parches/useCases/ListarMisParchesCreadosUseCase.java
+++ src/main/java/co/com/sofka/parches/useCases/ListarMisParchesCreadosUseCase.java
@@ -2,12 +2,14 @@ package co.com.sofka.parches.useCases;
 
 import co.com.sofka.parches.dtos.ParcheDTO;
 import co.com.sofka.parches.mappers.ParcheMapper;
+import co.com.sofka.parches.repositories.InscripcionRepository;
 import co.com.sofka.parches.repositories.ParcheRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 import reactor.core.publisher.Flux;
 
 import java.util.Objects;
+import java.util.function.Function;
 
 @Service
 @Validated
@@ -15,16 +17,30 @@ public class ListarMisParchesCreadosUseCase implements ListarMisParchesCreados {
 
     private final ParcheRepository parcheRepository;
     private final ParcheMapper parcheMapper;
+    private final InscripcionRepository inscripcionRepository;
 
-    public ListarMisParchesCreadosUseCase(ParcheRepository parcheRepository, ParcheMapper parcheMapper) {
+    public ListarMisParchesCreadosUseCase(ParcheRepository parcheRepository, ParcheMapper parcheMapper, InscripcionRepository inscripcionRepository) {
         this.parcheRepository = parcheRepository;
         this.parcheMapper = parcheMapper;
+        this.inscripcionRepository = inscripcionRepository;
     }
 
     @Override
     public Flux<ParcheDTO> listarMisParchesCreados(String duenoDelParche) {
         Objects.requireNonNull(duenoDelParche, "Id del dueño del parche es requerido");
         return parcheRepository.findAllByDuenoDelParche(duenoDelParche)
-                .map(parcheMapper.mapToDTO());
+                .map(parcheMapper.mapToDTO())
+                .flatMap(contarParticipantesParche());
+    }
+
+    private Function<ParcheDTO, Flux<ParcheDTO>> contarParticipantesParche() {
+        return parcheDto ->
+                Flux.just(parcheDto).zipWith(
+                        inscripcionRepository.findAllByParcheId(parcheDto.getId())
+                                .collectList(),
+                        (parche, inscripciones) -> {
+                            parche.setCantidadParticipantes(inscripciones.size());
+                            return parche;
+                        });
     }
 }
