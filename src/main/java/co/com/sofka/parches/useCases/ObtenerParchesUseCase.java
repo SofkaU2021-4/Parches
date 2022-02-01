c02356b12e7ef8e94dbe26750605da6e78d6a52d
Cristian Cabarcas <cridejecato@gmail.com>
2022-01-31 22:29:40 -0500

    Feat: Router and Use Case

--- src/main/java/co/com/sofka/parches/useCases/ObtenerParchesUseCase.java
+++ src/main/java/co/com/sofka/parches/useCases/ObtenerParchesUseCase.java
@@ -1,10 +1,29 @@
 package co.com.sofka.parches.useCases;
 
+import co.com.sofka.parches.dtos.ParcheDTO;
+import co.com.sofka.parches.mappers.ParcheMapper;
+import co.com.sofka.parches.repositories.InscripcionRepository;
+import co.com.sofka.parches.repositories.ParcheRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 @Validated
 public class ObtenerParchesUseCase {
+    private final ParcheRepository parcheRepository;
+    private final ParcheMapper parcheMapper;
+    private final InscripcionRepository inscripcionRepository;
 
+    public ObtenerParchesUseCase(ParcheRepository parcheRepository, ParcheMapper parcheMapper, InscripcionRepository inscripcionRepository) {
+        this.parcheRepository = parcheRepository;
+        this.parcheMapper = parcheMapper;
+        this.inscripcionRepository = inscripcionRepository;
+    }
+
+    public Flux<ParcheDTO> apply(){
+            return parcheRepository.findAll()
+                    .flatMap(parche -> Mono.just(parcheMapper.mapToDTO().apply(parche)));
+    }
 }
